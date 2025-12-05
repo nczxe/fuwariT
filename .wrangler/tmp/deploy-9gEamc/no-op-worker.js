080fcec39b775ebb072be0fff99526480b7880e4
nczxe <lujunyu0214@outlook.com>
2025-12-05 18:35:33 +0800

    #51205

---
+++ .wrangler/tmp/deploy-9gEamc/no-op-worker.js
@@ -0,0 +1,15 @@
+// C:/Users/lujun/AppData/Local/pnpm-cache/dlx/ac1a292d7f9dc4086c585bf07bccb41800a5d0c07957abd47b314fa5f46b222b/19aee0bd3a9-3774/node_modules/.pnpm/wrangler@4.53.0/node_modules/wrangler/templates/no-op-worker.js
+var no_op_worker_default = {
+  fetch() {
+    return new Response("Not found", {
+      status: 404,
+      headers: {
+        "Content-Type": "text/html"
+      }
+    });
+  }
+};
+export {
+  no_op_worker_default as default
+};
+//# sourceMappingURL=no-op-worker.js.map
